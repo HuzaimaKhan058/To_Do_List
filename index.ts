04f92dccef444b61c7d8757393cccf6aa62e0c1d
HuzaimaKhan058 <huzaimakhan058@gmail.com>
2024-04-07 00:30:41 +0500

    A cli based to-do list to manage your tasks.

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while(condition){
+     let answer = await inquirer.prompt(
+ [
+    {
+        name: "firstQuestion",
+        message:"What would you like to add in your todos?",
+        type: "input"
+    },
+    {
+        name: "secondQuestion",
+        message: "Would you like to add more in your todos?",
+        type: "confirm",
+        default: "true"
+    }
+ ]
+);
+
+todos.push(answer.firstQuestion);
+console.log(todos);
+condition = answer.secondQuestion;
+};
